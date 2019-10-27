ae1caebccff2df369fd92be232f527392e004269
AlexanderPuckhaber <alexpuckhaber@gmail.com>
2019-10-27 02:16:44 -0400

    good luck

---
+++ App.vue
@@ -0,0 +1,41 @@
+<script>
+  import Test from './Test.vue';
+
+  export default {
+    data: function() {
+      return {
+        text: "",
+        message: "Hello World"
+      };
+    },
+    methods: {
+      handleBtnPress: function() {
+        alert('Btn Press');
+      }
+    },
+    components: {
+      Test
+    }
+  }
+</script>
+
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+      <Test/>
+      <button v-bind:title="message" v-bind:on-press="handleBtnPress"/>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ Test.vue
@@ -0,0 +1,47 @@
+<template>
+    <view class="soft-container">
+        <text-input
+          class="text-input"
+          placeholder="What item are you looking for?"
+          v-model="text"
+          v-on:keyup.enter="submit"
+        />
+        <text>{{text}}</text>
+        <text>{{stuff}}</text>
+        <button title="Go" v-bind:on-press="submit" />
+    </view>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name:"Test",
+    data: {
+        text: "Banana",
+        stuff: "test2"
+    },
+    methods: {
+        submit: function (event) {
+            
+            let url = 'http://128.61.29.108:3000/api/merchants?s=' + this.text;
+            axios.get(url)
+            .then((response) => {
+                Vue.set(data.stuff, "ok");
+                Vue.set(data.stuff, response);
+                alert(response.name);
+            })
+            .catch(error => alert(error));
+        }
+    }
+}
+</script>
+
+<style>
+.soft-container {
+  background-color: #EEEEEE;
+}
+.text-input {
+    font-size: 30px;
+}
+</style>
